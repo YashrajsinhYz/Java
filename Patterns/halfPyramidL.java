7d18d08981e16765a0ae2146f78f57fdfe309b3f
YashrajsinhYz <97649747+YashrajsinhYz@users.noreply.github.com>
2022-08-13 13:44:44 +0530

    Add files via upload

---
+++ Patterns/halfPyramidL.java
@@ -0,0 +1,28 @@
+public class halfPyramidL {
+    public static void main(String[] args) {
+
+        int n=4;       
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if( (i+j) >= (n+1) )
+                System.out.print("* ");
+                
+                else
+                System.out.print("  ");
+            }
+
+            // for(int j=1; j <= (n-i); j++)
+            // {
+            //     System.out.print("  ");
+            // }
+            // for(int k=1; k<=i; k++)
+            // {
+            //     System.out.print("* ");
+            // }
+
+            System.out.println();
+        }
+
+    }
+}
